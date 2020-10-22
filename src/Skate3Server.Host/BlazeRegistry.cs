ea33d646fbd5cb6738b73bce8bebd843c53dbb7f
Morgan Creekmore <morgan@creekmore.email>
2020-10-21 23:53:21 -0500

    Fix DI

--- src/Skate3Server.Host/BlazeRegistry.cs
+++ src/Skate3Server.Host/BlazeRegistry.cs
@@ -16,6 +16,7 @@ namespace Skate3Server.Host
             builder.RegisterType<BlazeRequestHandler>().As<IBlazeRequestHandler>();
             builder.RegisterType<BlazeRequestParser>().As<IBlazeRequestParser>();
             builder.RegisterType<BlazeSerializer>().As<IBlazeSerializer>();
+            builder.RegisterType<BlazeDeserializer>().As<IBlazeDeserializer>();
             builder.RegisterType<BlazeDebugParser>().As<IBlazeDebugParser>();
             builder.RegisterType<BlazeTypeLookup>().As<IBlazeTypeLookup>();
             builder.RegisterType<Ps3TicketDecoder>().As<IPs3TicketDecoder>();
