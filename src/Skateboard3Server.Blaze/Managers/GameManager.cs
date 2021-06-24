a1a4deb9c8454ef9a294906d0273f698844d2200
Morgan Creekmore <morgan@creekmore.email>
2021-06-23 22:22:08 -0500

    Add GameManager

---
+++ src/Skateboard3Server.Blaze/Managers/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+
+namespace Skateboard3Server.Blaze.Managers
+{
+    public interface IGameManager
+    {
+    }
+
+    /// <summary>
+    /// Manages game sessions
+    /// </summary>
+    public class GameManager : IGameManager
+    {
+        private readonly ConcurrentDictionary<uint, GameData> _games =
+            new ConcurrentDictionary<uint, GameData>();
+
+        private int _currentGameCount = 1;
+
+    }
+
+    public class GameData
+    {
+    }
+}
