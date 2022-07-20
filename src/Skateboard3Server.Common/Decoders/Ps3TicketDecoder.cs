03c26cfaa652952b79974fc1901746649b5d4b90
Tom Delebo <t.delebo@comcast.net>
2022-07-19 21:39:26 -0500

    Add v2.0 and v4.0 ticket decoding support

--- src/Skateboard3Server.Common/Decoders/Ps3TicketDecoder.cs
+++ src/Skateboard3Server.Common/Decoders/Ps3TicketDecoder.cs
@@ -28,19 +28,27 @@ public class Ps3TicketDecoder : IPs3TicketDecoder
                 //Ticket Header
                 var majorVersion = (byte) (reader.ReadByte() >> 4);
                 var minorVersion = reader.ReadByte();
-                reader.ReadBytes(4); //00 00 00 00
-
-                var ticketLength = reader.ReadUInt16Be();
                 ticket.Header = new TicketHeader
                 {
                     MajorVersion = majorVersion,
                     MinorVersion = minorVersion
                 };
 
-                //TODO: add length check
+                reader.ReadBytes(4); //00 00 00 00
+
+                var ticketLength = reader.ReadUInt16Be();
+                if (ticketLength != (ticketData.Length - 8)) //subtract 8 because the header is 8 bytes long
+                {
+                    Logger.Debug($"Ticket length did not match data");
+                    return null;
+                }
+                
                 //TODO: check sections
-                //TODO: support 2.0 and 4.0
-                if (majorVersion == 2 && minorVersion == 1) //2.1
+                if (majorVersion == 2 && minorVersion == 0) //2.0
+                {
+                    ticket.Body = Read20Body(reader);
+                }
+                else if (majorVersion == 2 && minorVersion == 1) //2.1
                 {
                     ticket.Body = Read21Body(reader); 
                 }
@@ -48,11 +56,16 @@ public class Ps3TicketDecoder : IPs3TicketDecoder
                 {
                     ticket.Body = Read30Body(reader);
                 }
+                else if (majorVersion == 4 && minorVersion == 0) //4.0
+                {
+                    ticket.Body = Read40Body(reader);
+                }
                 else
                 {
                     Logger.Debug($"Unsupported ticket version {majorVersion}.{minorVersion}");
                     return null;
                 }
+
                 //TODO handle incomplete body reading
                 ticket.Footer = ReadFooter(reader); //footer
 
@@ -65,6 +78,63 @@ public class Ps3TicketDecoder : IPs3TicketDecoder
             Logger.Error($"Invalid Ticket {e}");
             return null;
         }
+    }
+
+    private TicketBody Read20Body(BinaryReader reader)
+    {
+        var body = new TicketBody();
+        var bodyHeader = ReadSectionHeader(reader);
+
+        //serial_id
+        var serialHeader = ReadValueHeader(reader);
+        body.SerialId = reader.ReadBytes(serialHeader.Length);
+
+        //issuer_id
+        var issuerHeader = ReadValueHeader(reader);
+        body.IssuerId = reader.ReadUInt32Be();
+
+        //issued_date
+        var issuedDateHeader = ReadValueHeader(reader);
+        body.IssuedDate = reader.ReadUInt64Be();
+
+        //expire_date
+        var expireDateHeader = ReadValueHeader(reader);
+        body.ExpireDate = reader.ReadUInt64Be();
+
+        //user_id
+        var userIdHeader = ReadValueHeader(reader);
+        body.UserId = reader.ReadUInt64Be();
+
+        //online_id
+        var usernameHeader = ReadValueHeader(reader);
+        //TODO: confirm utf8
+        body.Username = Encoding.ASCII.GetString(reader.ReadBytes(usernameHeader.Length)).TrimEnd('\0');
+
+        //region/lang
+        var regionHeader = ReadValueHeader(reader);
+        body.Region = Encoding.ASCII.GetString(reader.ReadBytes(3)).TrimEnd('\0');
+        body.Language = reader.ReadByte();
+
+        //domain
+        var domainHeader = ReadValueHeader(reader);
+        body.Domain = Encoding.ASCII.GetString(reader.ReadBytes(domainHeader.Length)).TrimEnd('\0');
+
+        //service_id
+        var serviceHeader = ReadValueHeader(reader);
+        body.ServiceId = reader.ReadBytes(serviceHeader.Length);
+
+        //age/status
+        var statusHeader = ReadValueHeader(reader);
+        body.Age = reader.ReadByte();
+        reader.ReadByte(); //00
+        body.Status = reader.ReadByte();
+        reader.ReadByte(); //00
+
+        //TODO ???
+        //unknown
+        reader.ReadBytes(8);
+
+        return body;
     }
 
     private TicketBody Read21Body(BinaryReader reader)
@@ -185,6 +255,82 @@ public class Ps3TicketDecoder : IPs3TicketDecoder
         //unknown
         var unknownHeader = ReadSectionHeader(reader);
         ReadValueHeader(reader);
+
+        return body;
+    }
+
+    private TicketBody Read40Body(BinaryReader reader)
+    {
+        var body = new TicketBody();
+        var bodyHeader = ReadSectionHeader(reader);
+
+        //serial_id
+        var serialHeader = ReadValueHeader(reader);
+        body.SerialId = reader.ReadBytes(serialHeader.Length);
+
+        //issuer_id
+        var issuerHeader = ReadValueHeader(reader);
+        body.IssuerId = reader.ReadUInt32Be();
+
+        //issued_date
+        var issuedDateHeader = ReadValueHeader(reader);
+        body.IssuedDate = reader.ReadUInt64Be();
+
+        //expire_date
+        var expireDateHeader = ReadValueHeader(reader);
+        body.ExpireDate = reader.ReadUInt64Be();
+
+        //user_id
+        var userIdHeader = ReadValueHeader(reader);
+        body.UserId = reader.ReadUInt64Be();
+
+        //online_id
+        var usernameHeader = ReadValueHeader(reader);
+        //TODO: confirm utf8
+        body.Username = Encoding.ASCII.GetString(reader.ReadBytes(usernameHeader.Length)).TrimEnd('\0');
+
+        //region/lang
+        var regionHeader = ReadValueHeader(reader);
+        body.Region = Encoding.ASCII.GetString(reader.ReadBytes(3)).TrimEnd('\0');
+        body.Language = reader.ReadByte();
+
+        //domain
+        var domainHeader = ReadValueHeader(reader);
+        body.Domain = Encoding.ASCII.GetString(reader.ReadBytes(domainHeader.Length)).TrimEnd('\0');
+
+        //service_id
+        var serviceHeader = ReadValueHeader(reader);
+        body.ServiceId = reader.ReadBytes(serviceHeader.Length);
+
+        //date_of_birth
+        var dobHeader = ReadSectionHeader(reader);
+        var dob = new DateOfBirth();
+        dob.Year = reader.ReadUInt16Be();
+        dob.Month = reader.ReadByte();
+        dob.Day = reader.ReadByte();
+        body.DateOfBirth = dob;
+
+        //age/status
+        var statusHeader = ReadValueHeader(reader);
+        body.Age = reader.ReadByte();
+        reader.ReadByte(); //00
+        body.Status = reader.ReadByte();
+        reader.ReadByte(); //00
+
+        //unknown
+        var unknownHeader = ReadSectionHeader(reader);
+        ReadValueHeader(reader);
+
+        //unknown
+        reader.ReadBytes(4);
+
+        //unknown String (8)
+        var unknownStringOne = ReadValueHeader(reader);
+        body.UnknownStringOne = Encoding.ASCII.GetString(reader.ReadBytes(unknownStringOne.Length)).TrimEnd('\0');
+
+        //unknown String (64)
+        var unknownStringTwo = ReadValueHeader(reader);
+        body.UnknownStringTwo = Encoding.ASCII.GetString(reader.ReadBytes(unknownStringTwo.Length)).TrimEnd('\0');
 
         return body;
     }
