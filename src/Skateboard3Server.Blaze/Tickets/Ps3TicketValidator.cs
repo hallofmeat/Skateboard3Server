96371f391b47c670b09f6c10a8d0209f413b812e
Malogn <noahmarcelh@gmail.com>
2023-02-06 00:14:38 +0100

    Add european Skate 3 game title IDs

--- src/Skateboard3Server.Blaze/Tickets/Ps3TicketValidator.cs
+++ src/Skateboard3Server.Blaze/Tickets/Ps3TicketValidator.cs
@@ -65,7 +65,7 @@ public class Ps3TicketValidator : IPs3TicketValidator
         }
 
         var serviceId = Encoding.ASCII.GetString(body.ServiceId);
-        if (!serviceId.Contains("BLUS30464"))
+        if (!(serviceId.Contains("BLUS30464") || serviceId.Contains("BLES00760")))
         {
             Logger.Warn($"Ticket with incorrect Serial:{serviceId}");
             return false;
